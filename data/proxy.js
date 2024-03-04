9a114456f94956020c837d9a1967ab437ad4332d
Bear <159610842+fghfdghgfd@users.noreply.github.com>
2024-03-03 17:55:17 -0800

    f

--- data/proxy.js
+++ data/proxy.js
@@ -5,6 +5,12 @@ var proxys = [
     'url': 'https://nowgg.nl/play/roblox-corporation/5349/roblox',
     'image': 'https://lh3.googleusercontent.com/vI2_gJPlrp1gv-b-LlgqhEXGudx4dAPH8eWlnVUbAE4wo0UZ9dTCCvHCHUgkUbZ9BB3rJ74mRTPf0AqyIOgJFK9itizXw0hZenpOLxUzXHi3FZEyLiL0h3Iv5y-1L5CuCA=w517'
   },
+  {
+    'id': 'interstellar',
+    'title': "Interstellar",
+    'url': 'https://nowgg.nl/play/roblox-corporation/5349/roblox',
+    'image': 'https://lh5.googleusercontent.com/PKPW-u5z5h1NZeFViEj_Fl0KbConp_MUfBTbBvZSuQ2AC8nWglDGGB2ewQbkfR_2I5hyfMow0WcFYY9qTuKtmyHWolBVL9tfT7r3UclTCYVywFtBfaek0kV0CEMsLLCAHw=w200'
+  },
 ]
 
 var customproxys = JSON.parse(localStorage.getItem("customproxys"))
