70498da18997ff1a7e29b036833d34e61540668d
Bear <159610842+fghfdghgfd@users.noreply.github.com>
2024-03-03 19:25:41 -0800

    f

--- data/proxy.js
+++ data/proxy.js
@@ -23,6 +23,12 @@ var proxys = [
     'url': 'https://starttiw.org/',
     'image': 'https://lh5.googleusercontent.com/GhbtVxVUUQe__NZPQmyYWPUPmzHovWeiNx8Veb_RJ2tbmcxLPMizIPW8OnYqIkSc7a7YK9GSfKhhoQuG7Y_lm2apt5bLhxR6dgvQm4I6z_No7sjPL8WYm6guz4hXt49CSw=w500'
   },
+  {
+    'id': 'polaris',
+    'title': "Polaris",
+    'url': 'https://polarislearning.org/',
+    'image': 'https://lh3.googleusercontent.com/RVG7kTQx1zfk8GPdNK43BsD2HkdQ0n_A4dqMbksiaCR50f9A_S_0ZLZJ9qqhZKT2JJkTJD1zHPCRVvKY4h1N63kfPViDt_34Lhlq1oBpPt4sSibL_Ml0cMHZYqn5lECjhw=w2560'
+  },
 ]
 
 var customproxys = JSON.parse(localStorage.getItem("customproxys"))
