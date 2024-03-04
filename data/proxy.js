968514d7c7aab34d9dff1998c8a2f54c1b6eb62d
Bear <159610842+fghfdghgfd@users.noreply.github.com>
2024-03-03 18:00:08 -0800

    f

--- data/proxy.js
+++ data/proxy.js
@@ -11,6 +11,12 @@ var proxys = [
     'url': 'https://nowgg.nl/play/roblox-corporation/5349/roblox',
     'image': 'https://lh5.googleusercontent.com/PKPW-u5z5h1NZeFViEj_Fl0KbConp_MUfBTbBvZSuQ2AC8nWglDGGB2ewQbkfR_2I5hyfMow0WcFYY9qTuKtmyHWolBVL9tfT7r3UclTCYVywFtBfaek0kV0CEMsLLCAHw=w200'
   },
+  {
+    'id': 'shuttle',
+    'title': "Shuttle",
+    'url': 'https://nowgg.nl/play/roblox-corporation/5349/roblox',
+    'image': 'https://lh5.googleusercontent.com/_wi0jWOc5DvPGWw8CmAm4qANRzeuUUpSXxR_bcRzmIRJ9MnCK8NctjyGkDcoeYhEK7GAIRmuozSmRJZWcj8jKsXHhdsPva_MBeKJOlbv9H2HBUL5W25Q2MxISWoRswgmcw=w1080'
+  },
 ]
 
 var customproxys = JSON.parse(localStorage.getItem("customproxys"))
