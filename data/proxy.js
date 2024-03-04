045b70fe1adea80d1d9cd3e9ab5eb038b6d59f76
Bear <159610842+fghfdghgfd@users.noreply.github.com>
2024-03-04 08:56:52 -0800

    g

--- data/proxy.js
+++ data/proxy.js
@@ -2,13 +2,13 @@ var proxys = [
   {
     'id': 'rammerhead',
     'title': "Rammerhead",
-    'url': 'https://nowgg.nl/play/roblox-corporation/5349/roblox',
+    'url': 'https://englishlessons.koti.cf/',
     'image': 'https://lh3.googleusercontent.com/vI2_gJPlrp1gv-b-LlgqhEXGudx4dAPH8eWlnVUbAE4wo0UZ9dTCCvHCHUgkUbZ9BB3rJ74mRTPf0AqyIOgJFK9itizXw0hZenpOLxUzXHi3FZEyLiL0h3Iv5y-1L5CuCA=w517'
   },
   {
     'id': 'interstellar',
     'title': "Interstellar",
-    'url': 'https://nowgg.nl/play/roblox-corporation/5349/roblox',
+    'url': 'https://educational-sooty.vercel.app/',
     'image': 'https://lh5.googleusercontent.com/PKPW-u5z5h1NZeFViEj_Fl0KbConp_MUfBTbBvZSuQ2AC8nWglDGGB2ewQbkfR_2I5hyfMow0WcFYY9qTuKtmyHWolBVL9tfT7r3UclTCYVywFtBfaek0kV0CEMsLLCAHw=w200'
   },
   {
